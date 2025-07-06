fd89ece955bcfee5e9cf5bfa290389e828767876
GregRos <life.gregr@gmail.com>
2025-07-06 23:03:49 +0300

    fix(ci): various issues with install oras script

--- scripts/_install.manifest.sh
+++ scripts/_install.manifest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-set -ex
+set -eux
+VERSION="1.2.3"
 tmp_dir="/tmp/install_oras"
 rm -rf $tmp_dir
 mkdir -p $tmp_dir
@@ -7,4 +8,4 @@ cd $tmp_dir
 curl -LO "https://github.com/oras-project/oras/releases/download/v${VERSION}/oras_${VERSION}_linux_amd64.tar.gz"
 mkdir -p oras-install/
 tar -zxf oras_${VERSION}_*.tar.gz -C oras-install/
-mv oras-install/oras /bin/
+mv oras-install/oras /bin/ || true
--- scripts/run.manifest.sh
+++ scripts/run.manifest.sh
@@ -2,8 +2,8 @@
 set -eux
 
 script_dir=$(cd "$(dirname "$0")" && pwd)
-chmod +x "$script_dir/_install-oras.sh"
-"$script_dir/_install-oras.sh"
+chmod +x "$script_dir/_install.manifest.sh"
+"$script_dir/_install.manifest.sh"
 
 CI_PROJECT_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
 CI_JOB_ID="${GITHUB_RUN_ID}"
@@ -25,7 +25,6 @@ yarn set version stable
 script_dir=$(cd "$(dirname "$0")" && pwd)
 chmod +x "$script_dir/_install.manifest.sh"
 "$script_dir/_install.manifest.sh"
-
 yarn workspaces focus k8s
 export IMAGE_SUFFIX="$SHORT_SHA"
 yarn workspace k8s run manifest
