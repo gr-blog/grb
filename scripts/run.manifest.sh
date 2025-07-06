6b9802f84c4468230722c49c9744c3a15054c8e0
GregRos <life.gregr@gmail.com>
2025-07-06 23:41:58 +0300

    fix(ci): install git during manifest, fix branch names

--- scripts/run.manifest.sh
+++ scripts/run.manifest.sh
@@ -2,8 +2,9 @@
 set -eux
 
 script_dir=$(cd "$(dirname "$0")" && pwd)
-chmod +x "$script_dir/_install.manifest.sh"
+chmod +x "$script_dir"/_*.manifest.sh
 "$script_dir/_install.manifest.sh"
+"$script_dir/_install.git.sh"
 
 CI_PROJECT_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
 CI_JOB_ID="${GITHUB_RUN_ID}"
