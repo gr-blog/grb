193df9c70f0b4eb61941ea1f7ce45451eaeb0917
GregRos <life.gregr@gmail.com>
2025-07-07 18:46:57 +0300

    feat(ci): use buildx cache ref, correct tag for images, remove old workflows

--- scripts/run.image.sh
+++ scripts/run.image.sh
@@ -13,22 +13,18 @@ fi
 if [ -z "${TARGET:-}" ]; then
     export TARGET="$SERVICE"
 fi
-grb_image() {
-    echo "name=registry.host/grb/grb:$1"
-}
-IMAGE_NAME="registry.host/grb/grb:$SERVICE"
+
+IMAGE_NAME="registry.host/grb/$SERVICE"
 export IMAGE_CANON="$IMAGE_NAME"
-export IMAGE_COMMIT="$IMAGE_NAME-$SHORT_SHA"
-if [ -f /laniakea/build.sh ]; then
-    sh /laniakea/build.sh \
-        --opt build-arg:SERVICE="$SERVICE" \
-        --opt target="$TARGET"
-else
-    docker buildx build \
-        --file Dockerfile \
-        --target "$TARGET" \
-        --build-arg SERVICE="$SERVICE" \
-        --tag "$IMAGE_NAME" \
-        --load \
-        .
-fi
+export IMAGE_COMMIT="$IMAGE_NAME:$SHORT_SHA"
+CACHE_IMAGE="$IMAGE_NAME:cache"
+docker buildx build \
+    --file Dockerfile \
+    --target "$TARGET" \
+    --build-arg SERVICE="$SERVICE" \
+    --tag "$IMAGE_NAME" \
+    --tag "$IMAGE_COMMIT" \
+    --cache-from type=registry,ref="$CACHE_IMAGE" \
+    --cache-to type=registry,ref="$CACHE_IMAGE" \
+    --push \
+    .
