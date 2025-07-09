0e99f83aebb30bc55d854c889133ec71efb8262d
GregRos <life.gregr@gmail.com>
2025-07-09 18:40:52 +0300

    refactor: rename textmate theme

--- packages/api/src/markdown/syntax-highlighting/theme.ts
+++ packages/api/src/markdown/syntax-highlighting/theme.ts
@@ -1,5 +1,5 @@
 import type { HighlighterGeneric } from "shiki/core"
-import rainbowBlack from "./rainbow-black-color-theme.json" with { type: "json" }
+import rainbowBlack from "./rainbow-black.shiki.json" with { type: "json" }
 export default async function theme(highlighter: HighlighterGeneric<any, any>) {
     await highlighter.loadTheme(rainbowBlack as any)
     return highlighter
