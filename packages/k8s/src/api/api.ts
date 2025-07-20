bf5c6a7cb3d4458cad23b87aae0f23e6aaefc3e9
GregRos <life.gregr@gmail.com>
2025-07-20 23:40:39 +0300

    fix(api): log level to debug

--- packages/k8s/src/api/api.ts
+++ packages/k8s/src/api/api.ts
@@ -28,7 +28,7 @@ export default W.Scope(namespace)
                     $env: {
                         DATA_SOURCE: "github",
                         BOT_API: addr,
-                        LOG_LEVEL: "warn",
+                        LOG_LEVEL: "debug",
                         API_KEY_MOOSEND: {
                             $ref: secret_moosend,
                             key: "API_KEY_MOOSEND"
